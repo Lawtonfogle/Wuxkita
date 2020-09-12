13d10f74b10894adc5fd5c0d7461ffc4b006407c
Adam Fogle <lawtonfogle@gmail.com>
2020-09-12 01:16:06 -0400

    Updates

---
+++ precache-manifest.8919349c262472327cd365dd7535459c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b4eaf298596a638b9c7f370cb145185",
+    "url": "/wuxkita/index.html"
+  },
+  {
+    "revision": "7a70a85706fafa701a7d",
+    "url": "/wuxkita/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cb46a1f56f343fc1b51e",
+    "url": "/wuxkita/static/js/2.497e3b09.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wuxkita/static/js/2.497e3b09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a70a85706fafa701a7d",
+    "url": "/wuxkita/static/js/main.98dafa48.chunk.js"
+  },
+  {
+    "revision": "48be544ec4e0632c4ee8",
+    "url": "/wuxkita/static/js/runtime-main.aa0e314d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wuxkita/static/media/logo.5d5d9eef.svg"
+  }
+]);
